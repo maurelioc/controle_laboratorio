407e53c8873812cad10afb70e5b72423dda4c0fb
marcoaurelio <marcoaurelio.castro@eb.mil.br>
2023-06-08 18:36:37 -0300

    Corrects bug in view and creates initial data

--- app/notas/views.py
+++ app/notas/views.py
@@ -1,24 +1,35 @@
 from datetime import datetime
 from django.shortcuts import render
 
-from .models import Fechamento, Trabalho, Cliente
+from .models import Fechamento, Trabalho
 
-# Create your views here.
+
 def gera_nota(request, nota_de_servico):
     trabalhos = Trabalho.objects.filter(fechamento__nota_de_servico=nota_de_servico)
     dentistas = sorted(list(set(t.cliente for t in trabalhos)), key=lambda x: x.nome)
+    clinicas = [c for d in dentistas for c in d.get_clinicas()]
+    clinica = max(set(clinicas), key=clinicas.count)
     context = {
         'grupos': {
             dentista: {
                 'trabalhos': {
-                    paciente: [t for t in trabalhos.filter(cliente=dentista).filter(paciente=paciente)]
-                    for paciente in (set(t.paciente for t in trabalhos.filter(cliente=dentista)))
+                    paciente: [
+                        t
+                        for t in trabalhos.filter(cliente=dentista).filter(
+                            paciente=paciente
+                        )
+                    ]
+                    for paciente in (
+                        set(t.paciente for t in trabalhos.filter(cliente=dentista))
+                    )
                 },
-                'total': sum(t.get_valor_total() for t in trabalhos.filter(cliente=dentista))
+                'total': sum(
+                    t.get_valor_total() for t in trabalhos.filter(cliente=dentista)
+                ),
             }
             for dentista in dentistas
         },
-        'clinica': dentistas[0].clinica,
+        'clinica': clinica,
         'hoje': datetime.now().date,
         'fechamento': Fechamento.objects.filter(nota_de_servico=nota_de_servico)[0],
     }
