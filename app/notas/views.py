3256b107ddc492210e7a324019f030b3b973afc7
marcoaurelio <marcoaurelio.castro@eb.mil.br>
2023-07-25 21:47:19 -0300

    Use new config vars

--- app/notas/views.py
+++ app/notas/views.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from django_admin_conf_vars.global_vars import config
 
 from .models import Fechamento, Trabalho
 
@@ -8,7 +10,7 @@ from .models import Fechamento, Trabalho
 def gera_nota(request, nota_de_servico):
     trabalhos = Trabalho.objects.filter(fechamento__nota_de_servico=nota_de_servico)
     if len(trabalhos) == 0:
-        return HttpResponseNotFound("Nota não encontrada")   
+        return HttpResponseNotFound("Nota não encontrada")
     dentistas = sorted(list(set(t.cliente for t in trabalhos)), key=lambda x: x.nome)
     clinicas = [c for d in dentistas for c in d.get_clinicas()]
     clinica = max(set(clinicas), key=clinicas.count)
@@ -35,6 +37,7 @@ def gera_nota(request, nota_de_servico):
         'clinica': clinica,
         'hoje': datetime.now().date,
         'fechamento': Fechamento.objects.filter(nota_de_servico=nota_de_servico)[0],
+        'config': config,
     }
 
     return render(request, 'gera_nota.html', context)
